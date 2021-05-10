eb46b60c0bbe7cbb19f4cbf43b52faa7d79c7fa1
levent karakuş <leventbkarakus@hotmail.com>
2021-05-10 19:53:48 -0400

    cors update, search function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,35 +3,36 @@ import './App.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
 import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      businesses: []
-    };
+		this.state = {
+			businesses: [],
+		};
 
-    this.searchYelp = this.searchYelp.bind(this);
-  }
+		this.searchYelp = this.searchYelp.bind(this);
+	}
 
-  searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(businesses => {
-      this.setState({businesses: businesses});
-    });
-  }
+	searchYelp(term, location, sortBy) {
+		Yelp.search(term, location, sortBy).then((businesses) => {
+			this.setState({
+				businesses: businesses,
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<h1>ravenous</h1>
+				<SearchBar searchYelp={this.searchYelp} />
+				<BusinessList businesses={this.state.businesses} />
+			</div>
+		);
+	}
 }
 
 export default App;
